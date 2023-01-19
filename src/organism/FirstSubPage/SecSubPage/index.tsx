e32817f04062b490735ebc87e28063ce2ec32e16
Musa Muhammad <musaawwaletudaye@gmail.com>
2023-01-19 23:58:46 +0100

    feat: building the grid-color page

--- src/organism/FirstSubPage/SecSubPage/index.tsx
+++ src/organism/FirstSubPage/SecSubPage/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SecSubPage = () => {
-  return <div className="grid grid-rows-3 gap-2"></div>;
+  return <div className="grid grid-rows-3 gap-3"></div>;
 };
 
 export default SecSubPage;
