76a89cefe87219115e9ab774fac1bee07ee14ddb
Musa Muhammad <musaawwaletudaye@gmail.com>
2023-06-16 22:56:19 +0100

    changed some paths

--- src/organism/ThirdSubPage/index.tsx
+++ src/organism/ThirdSubPage/index.tsx
@@ -6,7 +6,7 @@ const ThirdSubPage = () => {
     <div className="free">
       <p className="text-3xl font-sans leading-10">
         I offer a free consultation session to understand your needs, walk your
-        through my approach, and ensure that we're right fit for one another.
+        through my approach, and ensure that we are right fit for one another.
       </p>
       <div className=" md:ml-[40rem] ml-[5rem] w-[10rem] mt-7 md:pb-0 pb-5">
         <Button title="Free Session" />
