80bc1c105e85e583e7d4dea3984406ee18542579
Musa Muhammad <musaawwaletudaye@gmail.com>
2023-06-20 22:39:32 +0100

    changed some paths

--- src/organism/ThirdSubPage/index.tsx
+++ src/organism/ThirdSubPage/index.tsx
@@ -4,7 +4,7 @@ import "./ThirdSubPage.scss";
 const ThirdSubPage = () => {
   return (
     <div className="free">
-      <p className="text-3xl font-sans leading-10">
+      <p className="text-3xl font-sans leading-9">
         I give a free consultation session to understand your needs, walk your
         through my personal approach, and ensure that we are right fit for one
         another.
