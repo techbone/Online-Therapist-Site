941e95572353b3eb6c50b53e7992535cff607bc0
Musa Muhammad <musaawwaletudaye@gmail.com>
2023-06-25 23:53:25 +0100

    msde minor fixes

--- src/organism/ThirdSubPage/index.tsx
+++ src/organism/ThirdSubPage/index.tsx
@@ -6,7 +6,7 @@ const ThirdSubPage = () => {
     <div className="free">
       <p className="text-3xl font-sans leading-9">
         I give a free consultation session to understand your needs, walk your
-        through my personal approach, and ensure that we are right fit for one
+        through my personal approach and protocol, and ensure that we are right fit for one
         another.
       </p>
       <div className=" md:ml-[40rem] ml-[5rem] w-[10rem] mt-7 md:pb-0 pb-5">
